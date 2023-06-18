e807eebf142e12c69db711d3c0428d26b8c68942
AdamBear <adamxiong@gmail.com>
2023-06-18 11:45:57 +0800

    u

--- models/vlog.py
+++ models/vlog.py
@@ -35,7 +35,7 @@ class Vlogger :
         self.feature_extractor = FeatureExtractor(self.args)
         self.video_segmenter = VideoSegmentor(alpha=self.alpha, beta=self.beta)
         self.image_captioner = ImageCaptioner(model_name=self.args.captioner_base_model, device=self.args.image_captioner_device)
-        self.dense_captioner = DenseCaptioner(device=self.args.dense_captioner_device)
+        # self.dense_captioner = DenseCaptioner(device=self.args.dense_captioner_device)
         self.audio_translator = AudioTranslator(model=self.args.audio_translator, device=self.args.audio_translator_device)
         print('\033[1;32m' + "Model initialization finished!".center(50, '-') + '\033[0m')
     
@@ -77,12 +77,12 @@ class Vlogger :
             if ret:
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 image_caption = self.image_captioner.image_caption(frame)
-                dense_caption = self.dense_captioner.image_dense_caption(frame)
+                #dense_caption = self.dense_captioner.image_dense_caption(frame)
                 audio_transcript = self.audio_translator.match(audio_results, start_sec, end_sec)
                     
                 logger.info(f"When {format_time(start_sec)} - {format_time(end_sec)}")
                 logger.info(f"I saw {image_caption}.")
-                logger.info(f"I found {dense_caption}.")
+                #logger.info(f"I found {dense_caption}.")
                     
                 if len(audio_transcript) > 0:
                     logger.info(f"I heard someone say \"{audio_transcript}\"")
